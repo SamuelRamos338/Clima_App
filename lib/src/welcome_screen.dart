8371530939b755d174a0855b29853108621041c0
samuelramoscruz338 <samuelramoscruz338@gmail.com>
2025-03-13 23:59:46 +0000

    Clima_App2

--- lib/src/welcome_screen.dart
+++ lib/src/welcome_screen.dart
@@ -11,29 +11,38 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          spacing: 30.0,
-          children: [
-            SizedBox(height: 30),
-            Icon(Icons.lock, size: 45.0),
-            FlutterLogo(size: 90.0),
-            Text('Boas-vindas!'),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF7693ff),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: 
+          <Color>[Color(0xFF00457D), Colors.blue],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter
+        ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            spacing: 30.0,
+            children: [
+              SizedBox(height: 30),
+              Icon(Icons.lock, size: 45.0),
+              FlutterLogo(size: 90.0),
+              Text('Boas-vindas!'),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF7693ff),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Entrar', style: TextStyle(color: Colors.black)),
+                    Icon(Icons.arrow_forward, color: Colors.black),
+                  ],
+                ),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Entrar', style: TextStyle(color: Colors.black)),
-                  Icon(Icons.arrow_forward, color: Colors.black),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
